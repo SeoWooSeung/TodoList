61b817683c14ecd5a2f46c5811daa69d0dc73f98
Your Name <dntmd1531@naver.com>
2024-02-16 17:29:25 +0900

    000

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./style.css";
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState([]);
+
+  const [todoItm, setTodoItm] = useState({});
+
+  const num = useRef(0);
+
+  const todoUpdate = (e) => {
+    e.preventDefault();
+    if (todoItm.content.length < 2) {
+      alert("더입력해");
+      return;
+    }
+    setTodo([...todo, todoItm]);
+
+    setTodoItm({
+      content: "",
+    });
+
+    num.current = num.current + 1;
+  };
+
+  const inputHandler = (e) => {
+    const { value } = e.target;
+    setTodoItm({ id: num.current, content: value, done: false });
+  };
+
+  const done = (idx) => {
+    const r = todo.filter((it) => it.id !== idx);
+    setTodo(r);
+    console.log(idx);
+  };
+
+  const modify = (idx) => {
+    const r = todo.map((it) =>
+      it.id === idx
+        ? {
+            id: it.id,
+            content: it.content,
+            done: true,
+          }
+        : it
+    );
+    setTodo(r);
+  };
+
+  console.log(todo);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={todoUpdate} action="">
+        <input
+          type="text"
+          onChange={inputHandler}
+          value={todoItm.content || ""}
+        />
+        <button>todo</button>
+      </form>
+      <ol>
+        {todo.map((it, idx) => {
+          return (
+            <li className={it.done ? "on" : ""} key={it.id}>
+              {it.content}
+              <button onClick={() => modify(it.id)}>done</button>
+              <button onClick={() => done(it.id)}>delete</button>
+            </li>
+          );
+        })}
+      </ol>
+    </>
   );
-}
+};
 
 export default App;
